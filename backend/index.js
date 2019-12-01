1f22cefdaf9adb31ed21de169bde4a7bc55702ad
gebeto <slavik.nychkalo@gmail.com>
2019-12-01 10:59:46 +0200

    add static and public files connect with nginx

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use("/static", express.static("static"))
+
 app.get("/", (req, res) => {
+	res.send(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><title>Document</title></head>
+	<body>
+		<link rel="stylesheet" href="/static/styles.css" />
+		
+		<h1>Hello world</h1>
+
+		<div id="root">Loading...</div>
+
+		<script src="/static/js/bundle.js"></script>
+	</body>
+</html>`);
+});
+
+app.get("/api", (req, res) => {
 	res.json({
 		message: "Done!",
 	});
